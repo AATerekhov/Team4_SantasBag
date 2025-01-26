0299e8cff83c3172ca047afe8ec97956e609aeb4
AATerekhov <A.A.Terekhov@mail.ru>
2025-01-26 23:47:02 +0300

    Started Tests Adding AutoDataAttribute

---
+++ SantasBag.UnitTests/Helps/AutoMoqDataAttribute.cs
@@ -0,0 +1,19 @@
+﻿using AutoFixture;
+using AutoFixture.AutoMoq;
+using AutoFixture.Xunit2;
+using SantasBag.BusinessLogic.Services;
+
+namespace SantasBag.UnitTests.Helps
+{
+    public class AutoMoqDataAttribute : AutoDataAttribute
+    {
+        public AutoMoqDataAttribute() : base(fixtureFactory: fixtureFactory)
+        { }
+        private static readonly Func<IFixture> fixtureFactory = () =>
+        {
+            var fixture = new Fixture().Customize(new AutoMoqCustomization());
+            fixture.Customize<RewardsService>(c => c.OmitAutoProperties());
+            return fixture;
+        };
+    }
+}
